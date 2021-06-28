179d2da9d82046bb31b1631dbe4505ce5c369d46
aussiearef <arefkr@gmail.com>
2021-06-28 14:05:20 +1000

    Getting Kafka's metadata

---
+++ Microservices-Kafka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace Microservices_Kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string server = "127.0.0.1:9092";
+            const string topicName = "kafka-test";
+
+            var adminClient = new Confluent.Kafka.AdminClientBuilder(
+                new AdminClientConfig
+                {
+                    BootstrapServers = server,
+                    SecurityProtocol = SecurityProtocol.Plaintext
+                }
+                ).Build();
+
+            var metaData = adminClient.GetMetadata(TimeSpan.FromMinutes(1));
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
